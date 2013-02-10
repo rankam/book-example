f42f130f334d355c68318de139fcaed966a68052
Harry <hjwp2@cantab.net>
2013-02-10 05:55:16 +0000

    minimal view in urls.py

--- awesomelists/urls.py
+++ awesomelists/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+HOME_PAGE = '''
+<html>
+    <head><title>To-Do App</title></head>
+</html>'''
+
+def get_home_page(request):
+    return HttpResponse(HOME_PAGE)
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'awesomelists.views.home', name='home'),
+    url(r'^$', get_home_page),
     # url(r'^awesomelists/', include('awesomelists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
