998c937ed7de6af34b1d657913592b252e763878
Harry <hjwp2@cantab.net>
2013-02-14 00:23:31 +0000

    First unit test and url mapping, dummy view

--- awesomelists/urls.py
+++ awesomelists/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'awesomelists.views.home', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
     # url(r'^awesomelists/', include('awesomelists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
