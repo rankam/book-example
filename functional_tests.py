893d48e471784671fb0e23dd553a19872410b576
Harry <hjwp2@cantab.net>
2013-03-26 22:46:37 +0000

    Extend FT with better error messages, look for second row.

--- functional_tests.py
+++ functional_tests.py
@@ -40,12 +40,25 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+            "New to-do item did not appear in table -- its text was:\n%s" % (
+                table.text,
+            )
         )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '2: Use peacock feathers to make a fly' for row in rows),
+            "New to-do item did not appear in table -- its text was:\n%s" % (
+                table.text,
+            )
+        )
         self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
