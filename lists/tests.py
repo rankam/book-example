e4888d50f355947a51b07bbc948eddee808d7cbe
Harry <hjwp2@cantab.net>
2013-03-27 10:42:02 +0000

    First model unit test, and placeholder models.

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -33,3 +34,36 @@ class HomePageTest(TestCase):
             {'new_item_text':  'A new list item'}
         )
         self.assertEqual(response.content, expected_html)
+
+
+class ListsAndItemsModelsTest(TestCase):
+
+    def test_creating__saving_and_retrieving_a_list_with_items(self):
+        new_list = List()
+        new_list.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = new_list
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = new_list
+        second_item.save()
+
+        saved_lists = List.objects.all()
+        self.assertEqual(saved_lists.count(), 1)
+        saved_list = saved_lists[0]
+        self.assertEqual(saved_list.id, new_list.id)
+        self.assertEqual(saved_list, new_list)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, saved_list)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, saved_list)
