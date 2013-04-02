cc279d86d315e29646c6523b8a5aea8b16360bcd
Harry <hjwp2@cantab.net>
2013-04-02 23:25:37 +0100

    tweak ft to properly triangulate second list item appearing.

--- functional_tests.py
+++ functional_tests.py
@@ -59,11 +59,11 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.fail('Finish the test!')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generate a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
