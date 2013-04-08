91ec2afa937ae68733e14fd0b0038f9ed891af04
Harry <hjwp2@cantab.net>
2013-04-08 08:01:51 +0100

    New model for lists.

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
-    text = models.CharField(max_length=200)
+    text = models.TextField()
+    list = models.ForeignKey(List)
