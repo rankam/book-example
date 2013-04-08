decd3eec460e56744ca7e3c3e979a455022b5517
Harry <hjwp2@cantab.net>
2013-04-08 10:09:19 +0100

    remove pointless newline

--- superlists/urls.py
+++ superlists/urls.py
@@ -8,8 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/(.+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^lists/(.+)/new_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'
-    ),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     # url(r'^superlists/', include('superlists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
