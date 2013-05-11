7286906fe7e7d902ee376d36b90bd4612d8e45eb
Harry <hjwp2@cantab.net>
2013-05-11 23:32:05 +0100

    fist steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,5 +88,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
 
+    def test_layount_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
 
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_tag_name('input')
+        window_width = self.browser.get_window_size()['width']
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            window_width / 2,
+            delta=3
+        )
+        self.fail("check it's also centered on other pages")
+        self.fail("check footer copyright message is at bottom of page")
 
