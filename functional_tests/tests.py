dd89dbb81ef87692775e94719d75a0f6005ee779
Harry <hjwp2@cantab.net>
2013-05-12 00:22:31 +0100

    refactor templates to use a base template and extend ft

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -100,6 +100,18 @@ class NewVisitorTest(LiveServerTestCase):
             window_width / 2,
             delta=3
         )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_tag_name('input')
+        window_width = self.browser.get_window_size()['width']
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            window_width / 2,
+            delta=3
+        )
+
         self.fail("check it's also centered on other pages")
         self.fail("check footer copyright message is at bottom of page")
 
