27aaf0818a30ce5be48e14834fddbef9daeb0110
Harry <hjwp2@cantab.net>
2013-05-27 09:52:10 +0100

    set STATIC_ROOT and ALLOWED_HOSTS in settings, switch DEBUG to False

--- superlists/settings.py
+++ superlists/settings.py
@@ -1,6 +1,6 @@
 # Django settings for superlists project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -56,7 +56,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+from os import path
+STATIC_ROOT = path.abspath(path.join(path.dirname(__file__), '../../static'))
+
+# This next setting is needed when DEBUG=False
+ALLOWED_HOSTS = ['localhost']
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -151,3 +155,4 @@ LOGGING = {
         },
     }
 }
+
