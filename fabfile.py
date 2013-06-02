52c100f32ba5df87ee978230fb104ff518a24523
Harry <hjwp2@cantab.net>
2013-06-02 17:42:33 +0100

    tweaks to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,47 +1,53 @@
 from fabric.contrib.files import exists
-from fabric.api import cd, run
+from fabric.api import env, run
 from os import path
 
 
 REPO_URL = 'https://github.com/hjwp/book-example.git'
 SITES_FOLDER = '/home/harry/sites'
 
-def create_directory_structure(SITES_FOLDER, site_name):
+def deploy():
+    _create_directory_structure_if_necessary(env.host)
+    source_folder = path.join(SITES_FOLDER, env.host, 'source')
+    _get_latest_source(source_folder)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+
+def _create_directory_structure_if_necessary(site_name):
     base_folder = path.join(SITES_FOLDER, site_name)
     run('mkdir -p %s' % (base_folder))
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
         run('mkdir -p %s/%s' % (base_folder, subfolder))
 
 
-def get_latest_source(site_name):
-    source_folder = path.join(SITES_FOLDER, site_name, 'source')
+def _get_latest_source(source_folder):
     if exists(path.join(source_folder, '.git')):
-        with cd(source_folder):
-            run('git pull')
-            run('git reset --hard')
+        run('cd %s && git pull' % (source_folder,))
+        run('cd %s && git reset --hard' % (source_folder,))
     else:
-        with cd(source_folder):
-            run('git clone %s %s' % (REPO_URL, source_folder))
+        run('git clone %s %s' % (REPO_URL, source_folder))
 
 
-def update_virtualenv(site_name):
-    source_folder = path.join(SITES_FOLDER, site_name, 'source')
-    with cd(source_folder):
-        if not exists(path.join(source_folder, '../virtualenv')):
-            run('virtualenv ../virtualenv')
-        run('../virtualenv/bin/pip install -r requirements.txt')
+def _update_virtualenv(source_folder):
+    virtualenv_folder = path.join(source_folder, '../virtualenv')
+    if not exists(path.join(virtualenv_folder, 'bin', 'pip')):
+        run('virtualenv %s' % (virtualenv_folder,))
+    run('%s/bin/pip install -r %s/requirements.txt' % (
+            virtualenv_folder, source_folder
+    ))
 
 
+def _update_static_files(source_folder):
+    run('cd %s && python manage.py collectstatic --noinput' % (
+        source_folder,
+    ))
 
-def update_static_files(site_name):
-    source_folder = path.join(SITES_FOLDER, site_name, 'source')
-    with cd(source_folder):
-        run('python manage.py collectstatic --noinput')
 
-
-def update_database(site_name):
-    source_folder = path.join(SITES_FOLDER, site_name, 'source')
-    with cd(source_folder):
-        run('python manage.py syncdb --noinput')
+def _update_database(source_folder):
+    run('cd %s && python manage.py syncdb --noinput' % (
+        source_folder,
+    ))
 
 
